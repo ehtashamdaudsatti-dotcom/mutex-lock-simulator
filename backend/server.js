cadd6b7115810bf576d1d135166cc3c1dc7be60b
Ehtasham Daud Satti <ehtashamdaudsatti@gmail.com>
2026-07-23 03:24:34 -0700

    Add netlify config for frontend

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,10 @@ app.use("/api/mutex", mutexRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`✅ Server running on http://localhost:${PORT}`);
+  });
+}
 
 module.exports = app;
